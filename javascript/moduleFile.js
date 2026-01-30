5563c7600b47b546443b72800ab1692c551dbef6
Ashvin Parmar <ashvin.parmar@icreativetechnolabs.com>
2026-01-30 19:11:07 +0530

    javascript asynchronous/class/domnavigation/callback/timer/module working

---
+++ javascript/moduleFile.js
@@ -0,0 +1,9 @@
+function message(){
+    let name="Ashvin";
+    let age=21;
+    return "my name is " + name +" and age is " + age;
+}
+export function add(a,b){
+    return a+b;
+}
+export default message;
